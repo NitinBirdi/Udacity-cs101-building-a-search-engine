875741dac5d73779acf1bacf8fefe7f91ce2e787
Fouad Developer <fouad.b.business@gmail.com>
2017-04-04 03:01:13 -0400

    Solving this quiz with  a different approach
    using an opposite way of solving it with a while loop and an if statement

--- Problem Sets/Superhero nuisance.py
+++ Problem Sets/Superhero nuisance.py
@@ -41,3 +41,14 @@ print "Test case 1: ", fix_machine('UdaciousUdacitee', 'Udacity') == "Give me so
 print "Test case 2: ", fix_machine('buy me dat Unicorn', 'Udacity') == 'Udacity'
 print "Test case 3: ", fix_machine('AEIOU and sometimes y... c', 'Udacity') == 'Udacity'
 print "Test case 4: ", fix_machine('wsx0-=mttrhix', 't-shirt') == 't-shirt'
+
+### Solving with different approach:
+
+def fix_machine(debris, product):
+    i = 0
+    while i < len(product):
+        if debris.find(product[i]) != -1:
+            i += 1
+        else:
+            return "Give me something that's not useless next time."
+    return product
